120f35523004bcd2e12f4e20c28937985bc8102a
ponzimatej <matej.bedna@gmail.com>
2024-11-11 13:43:05 +0100

    added questions from macbook

--- coursecareers/javascript/questions/more questions/question12.js
+++ coursecareers/javascript/questions/more questions/question12.js
@@ -1,1 +1,23 @@
-// macbook
+function mergeSortedArrays(arr1, arr2) {
+  const sortedArr = [];
+
+  while (arr1.length + arr2.length > 0) {
+    let x = arr1.shift();
+    let y = arr2.shift();
+    if (x === undefined && y === undefined) break;
+    if (x <= y || y == undefined) {
+      sortedArr.push(x);
+      arr2.unshift(y);
+    }
+    if (y < x || x == undefined) {
+      sortedArr.push(y);
+      arr1.unshift(x);
+    }
+  }
+  console.log(sortedArr);
+}
+
+mergeSortedArrays(
+  [-10, 1, 2, 3, 4, 50, 60, 70, 100, 110],
+  [-5, 5, 15, 25, 35, 45, 55, 65, 75, 85, 95, 105, 115, 125]
+);
--- coursecareers/javascript/questions/more questions/question13.js
+++ coursecareers/javascript/questions/more questions/question13.js
@@ -1,1 +1,16 @@
-// macbook
+function updateEmployeeSalary(employees, employeeId, newSalary) {
+  let person;
+  for (employee of employees) {
+    if (employee.id === employeeId) {
+      person = this.employee;
+    }
+  }
+
+  if (person === undefined) {
+    console.log(null);
+    return;
+  }
+
+  person.salary = newSalary;
+  console.log(person);
+}
