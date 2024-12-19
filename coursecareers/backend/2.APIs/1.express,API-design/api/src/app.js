65eedace8b8436f2de1c2b5461fde7bdc9d9cb75
ponzimatej <matej.bedna@gmail.com>
2024-12-19 13:15:59 +0100

    APIs

--- coursecareers/backend/2.APIs/1.express,API-design/api/src/app.js
+++ coursecareers/backend/2.APIs/1.express,API-design/api/src/app.js
@@ -1,49 +1,21 @@
 import express from "express";
 import Database from "better-sqlite3";
+import favorites from "./routes/favorites.js";
+
+const db = new Database("favorites.db");
 
 // create our app
 const app = express();
 const port = 3000;
+app.use(express.json()); // make the app be able to receive json in the body
 
-const db = new Database("favorites.db");
+app.use("/favorites", favorites); // use the '/favorites' calls in /routes/favorites.js
+
+app.use((err, req, res, next) => {
+  console.log(err);
+});
 
 // start our app
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
-});
-
-// get json data - ("/favorites" is http://localhost:3000/favorites)
-app.get("/favorites", (request, response) => {
-  let query = "SELECT * FROM favorites";
-
-  // if there is a sort query in the request, sort it
-  const sort = request.query.sort;
-  if (sort === "asc") {
-    query += " ORDER BY name ASC";
-  }
-  if (sort === "desc") {
-    query += " ORDER BY name DESC";
-  }
-
-  const favorites = db.prepare(query).all();
-
-  response.json({ favorites });
-});
-
-app.get("/favorites/:id", (req, res) => {
-  try {
-    const id = parseInt(req.params.id); // get the ID from /:id
-
-    const favorite = db.prepare("SELECT * FROM favorites WHERE id = ?").get(id);
-    // don't append data directly from a user => dont do 'SELECT * FROM favorites WHERE id =' + id
-
-    if (!favorite) {
-      return res.status(404).json({ error: "Favorite not found." });
-    }
-
-    res.json({ favorite });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Something went wrong." });
-  }
 });
